14b34137195dfd9210ebc68a18cbf7d50591aff2
ngovantin <tindeptry1357@gmail.com>
2025-04-11 23:56:28 +0700

    fix error

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import app from './src/app.js';
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res)=>{
-  res.send('This is TINN api!')
+  res.send('This is TINN api!');
 })
 
 app.listen(port, () => {
