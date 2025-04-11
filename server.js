18cdbb781fc4006e982358eafeaef2b2f9961b83
ngovantin <tindeptry1357@gmail.com>
2025-04-11 23:01:25 +0700

    vercel

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
 
+app.use('/', (req, res)=>{
+  res.send('This is TINN api!')
+})
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
